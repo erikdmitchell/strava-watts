a06a57dd639f93b197ab7246a757f196eb596898
Erik <erikdmitchell@gmail.com>
2021-09-04 11:23:53 -0400

    font awesome is just that

--- includes/class-stwatt-shortcode.php
+++ includes/class-stwatt-shortcode.php
@@ -22,9 +22,7 @@ class STWATT_Shortcode {
      * @return void
      */
     public function scripts_styles() {
-        // https://developers.google.com/fonts/docs/material_icons
-        // https://fonts.google.com/icons
-        wp_enqueue_style( 'stwatt-google-icons-style', 'https://fonts.googleapis.com/icon?family=Material+Icons', '', '4.0.0' );
+        wp_enqueue_script( 'stwatt-fa-script', 'https://kit.fontawesome.com/f866e14327.js', '', '5.5.14', true ); 
     }
 
     /**
