3a790c53284bfe29799b38ffcec80127072299ac
Erik <erikdmitchell@gmail.com>
2021-09-07 11:55:29 -0400

    built out shortcode to look like a computer

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1,37 +1,28 @@
 <?php $athlete = stwatt_athlete(); ?>
-  
-<div class="stwatt-wrapper">
-    <div class="row">
-        <div class="col-xs-4 time">
-            <div class="clock">
-                <?php stwatt_str_wrap($athlete->stats->time, '<span>', '</span>'); ?>
-            </div>
-        </div>
-        <div class="col-xs-2 elevation">
-            <i class="fas fa-building"></i>
-        </div>
-        <div class="col-xs-4 distance">        
-            <div class="odometer">
-                <?php stwatt_str_wrap($athlete->stats->distance, '<span class="digit">', '</span>'); ?>
-            </div>
-        </div>
-    </div>
-</div>
 
-<div class="computer">
-    <div class="row1 roww">
-        <div class="data">
-            Data
+<div class="computer-wrapper">
+    <div class="computer">
+        <div class="computer-row">
+            <div class="data align-center text-uppercase">2021 Stats</div>
         </div>
-    </div>
-    <div class="row2 roww">
-        <div class="data">
-            Data
+        <div class="computer-row">
+            <div class="data-wrap">
+                <div class="data-label">Time</div>
+                <div class="data"><?php stwatt_str_wrap($athlete->stats->time, '<span>', '</span>'); ?></div>
+            </div>
         </div>
-    </div>
-    <div class="row2 roww">
-        <div class="data">
-            Data
-        </div>
-    </div>    
+        <div class="computer-row">
+            <div class="data-wrap">
+                <div class="data-label">Miles</div>
+                <div class="data"><?php stwatt_str_wrap($athlete->stats->distance, '<span>', '</span>'); ?></div>
+            </div>
+        </div> 
+
+        <div class="computer-row">
+            <div class="data-wrap">
+                <div class="data-label">Elev</div>
+                <div class="data"><?php stwatt_str_wrap($athlete->stats->elevation, '<span>', '</span>'); ?></div>
+            </div>
+        </div> 
+    </div>   
 </div>
